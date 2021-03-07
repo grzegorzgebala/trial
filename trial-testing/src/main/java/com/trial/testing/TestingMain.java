bd28cd488088d5fdee271057d7fd28b3d7ddda42
Grzegorz <grzegorz.gebala@yahoo.com>
2021-03-08 00:29:15 +0100

    Traial with junit

--- trial-testing/src/main/java/com/trial/testing/TestingMain.java
+++ trial-testing/src/main/java/com/trial/testing/TestingMain.java
@@ -1,29 +1,7 @@
 package com.trial.testing;
 
-import com.trial.testing.calculator.Calculator;
-import com.trial.testing.user.SimpleUser;
-
 public class TestingMain {
     public static void main(String[] args){
-        SimpleUser simpleUser = new SimpleUser("theForumUser");
 
-        String result = simpleUser.getUsername();
-
-        if (result.equals("theForumUser")){
-            System.out.println("test OK");
-        } else {
-            System.out.println("Error!");
-        }
-
-        // Testing Calculator class
-        Calculator calculator = new Calculator();
-
-        int addResult = calculator.add(180, 19);
-        int subResult = calculator.subtract(200, 150);
-        if ((addResult == 199) && (subResult == 50)) {
-            System.out.println("Calculator test OK");
-        } else {
-            System.out.println("Calculator has error!");
-        }
     }
 }
